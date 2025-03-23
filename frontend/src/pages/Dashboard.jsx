2b3f550ad9083ca0d40f26607bc456ff3fbf4be1
Mohammad Kaif Shaikh <mohammadkaif6268@gmail.com>
2025-03-23 11:55:18 +0530

    ok

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -149,7 +149,7 @@ function Dashboard() {
 
       {loading && (
         <div className="flex justify-center items-center mt-8">
-          <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-indigo-600">
+          <div className="rounded-full h-8 w-8 border-b-2 border-indigo-600">
             Employee Loading......
           </div>
         </div>
